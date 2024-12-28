c6bb6e7a053898a40e505b44cc43898d150670cf
Ivanelle Hunt <ivanellep@gmail.com>
2024-12-28 13:36:19 -0800

    graph showing without 404 error'

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,6 +5,8 @@ import { useLocalStorage } from '../hooks/useLocalStorage.js';
 
 const AppContext = createContext({});
 
+const BASE_URL = "https://hrf-asylum-be-b.herokuapp.com/cases";
+
 /**
  * TODO: Ticket 2:
  * - Use axios to fetch the data
@@ -17,16 +19,30 @@ const useAppContextProvider = () => {
 
   useLocalStorage({ graphData, setGraphData });
 
-  const getFiscalData = () => {
+  const getFiscalData = async () => {
     // TODO: Replace this with functionality to retrieve the data from the fiscalSummary endpoint
-    const fiscalDataRes = testData;
-    return fiscalDataRes;
+      try {
+        const response = await axios.get(`${BASE_URL}/fiscalSummary`);
+        if (!response.data.yearResults) {
+          response.data.yearResults = [];
+        }
+        return response.data;
+
+      } catch (error) {
+        console.error("Trouble fetching fiscal data:", error)
+        return null
+      }
   };
 
   const getCitizenshipResults = async () => {
     // TODO: Replace this with functionality to retrieve the data from the citizenshipSummary endpoint
-    const citizenshipRes = testData.citizenshipResults;
-    return citizenshipRes;
+    try {
+      const response = await axios.get(`${BASE_URL}/citizenshipSummary`)
+      return response.data;
+    } catch (error) {
+      console.error("Trouble fetching citizenship results", error)
+      return null
+    }
   };
 
   const updateQuery = async () => {
@@ -35,13 +51,34 @@ const useAppContextProvider = () => {
 
   const fetchData = async () => {
     // TODO: fetch all the required data and set it to the graphData state
-  };
+    try {
+      const [fiscalData, citizenshipData] = await Promise.all([
+        getFiscalData(),
+        getCitizenshipResults()
+      ]);
+
+      if (fiscalData && citizenshipData) {
+        setGraphData({
+          ...fiscalData,
+          citizenshipResults: citizenshipData
+        });
+    }
+  } catch (error) {
+    console.error("Error fetching data", error);
+  } finally {
+    setIsDataLoading(false);
+  }
+};
 
   const clearQuery = () => {
     setGraphData({});
   };
 
-  const getYears = () => graphData?.yearResults?.map(({ fiscal_year }) => Number(fiscal_year)) ?? [];
+  const getYears = () => {
+    if (!graphData?.yearResults) return [];
+    return graphData?.yearResults?.map(({ fiscal_year }) => Number(fiscal_year));
+  }
+    
 
   useEffect(() => {
     if (isDataLoading) {
