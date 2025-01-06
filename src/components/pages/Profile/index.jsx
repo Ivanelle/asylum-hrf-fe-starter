b549efa3c21ae84052d0c12008c907d6fc488cda
Ivanelle Hunt <ivanellep@gmail.com>
2025-01-05 19:16:26 -0800

    finished without styling 404 message

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,4 +1,6 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
 
 /**
  * TODO: Ticket 3:
@@ -12,10 +14,16 @@ const Profile = () => {
   const {
     isLoading,
     error,
-    user
+    user,
+    logout
   } = useAuth0();
+  const navigate = useNavigate();
 
-  if (isLoading || !user) {
+  const handleLogout = () => {
+    logout()
+  }
+
+  if (isLoading) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
@@ -23,13 +31,32 @@ const Profile = () => {
     return <div className='text-center p-4'>Oops... {error.message}</div>
   }
 
+  if (!user) {
+    navigate('/404');
+  }
+
   return (
-    <div>
-      <img src={user.picture} alt="Profile Picture" />
-      <h1 className="bold">{user.name}</h1>
-      <p>{user.email}</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-white p-6 rounded-lg shadow-md mx-auto w-96"> 
+        <div className="flex justify-center items-center"> 
+          <img src={user.picture} alt="Profile Picture" className="w-24 h-24 rounded-full mb-4" />
+        </div>
+        <h1 className="text-2xl font-bold mb-2">{user.name}</h1>
+        <p className="text-gray-500">{user.email}</p>
+        <button className="mt-4 
+          bg-blue-500 
+          hover:bg-blue-700 
+          text-white 
+          font-bold 
+          py-2 px-4 
+          rounded"
+          onClick={handleLogout}
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Profile;
+export default Profile
